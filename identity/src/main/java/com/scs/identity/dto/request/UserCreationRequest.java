cf4eb2176da27c812cb8c192473a9bb474e3ea1e
huynhanx03 <huynhnhandanang@gmail.com>
2024-12-11 09:33:27 +0700

    Add Exception and Validation

--- identity/src/main/java/com/scs/identity/dto/request/UserCreationRequest.java
+++ identity/src/main/java/com/scs/identity/dto/request/UserCreationRequest.java
@@ -1,7 +1,13 @@
 package com.scs.identity.dto.request;
 
+import jakarta.validation.constraints.Size;
+
 public class UserCreationRequest {
+
+    @Size(min = 8, max = 50, message = "Username must be at least 8 characters and no more than 50 characters long")
     private String username;
+
+    @Size(min = 8, max = 50, message = "Password must be at least 8 characters")
     private String password;
 
     public String getUsername() {
