3952032a3597932e0c56c378cb7358abf7d68a9c
huynhanx03 <huynhnhandanang@gmail.com>
2024-12-17 09:06:09 +0700

    Add eureka

---
+++ college-exam-scores/src/test/java/com/scs/collegeexamscores/CollegeExamScoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scs.collegeexamscores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeExamScoresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
